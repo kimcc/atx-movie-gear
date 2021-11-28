ccfe6d6479f7cbe74c3a97ac9da9621ba68de873
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-11-28 01:32:49 -0600

    working row and centered image on card

--- client/src/pages/DummyProduct.js
+++ client/src/pages/DummyProduct.js
@@ -1,69 +1,108 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+import { Card, Button, Row, Col, Container } from 'react-bootstrap';
+import '../styles.css';
 
 function DummyProduct(){
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
-      });
-      const token = mutationResponse.data.login.token;
-      Auth.login(token);
-    } catch (e) {
-      console.log(e);
-    }
-  };
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+
 
   return (
     <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+      <Link to="/detail">More</Link>
+      <h1>Cameras</h1>
+    <Container>
+      <Row>
+        <Col className="productCard">
+          <Card border="warning" style={{ width: '18rem' }}>
+            <div className="container">
+              <img className="productImage" justifyContent="center" style={{ width: '14rem' }} variant="top" src="/images/SONY_PXW-Fx9_XDCAM.jpeg" />
+            </div>
+            <Card.Body>
+              <Card.Title>Sony PWX Fx9 XDCAm</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="dark">See Product Details</Button>
+              <footer id="price">$95/day</footer>
+            </Card.Body>
+          </Card>
+        </Col>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/SONY_PXW-Fx9_XDCAM.jpeg" />
+            <Card.Body>
+              <Card.Title>Card Title 2</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make udarp the bulk of
+                the card's content.
+              </Card.Text>
+
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/SONY_PXW-Fx9_XDCAM.jpeg" />
+            <Card.Body>
+              <Card.Title>Card Title 3</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/SONY_PXW-Fx9_XDCAM.jpeg" />
+            <Card.Body>
+              <Card.Title>Card Title 3</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/SONY_PXW-Fx9_XDCAM.jpeg" />
+            <Card.Body>
+              <Card.Title>Card Title 3</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/SONY_PXW-Fx9_XDCAM.jpeg" />
+            <Card.Body>
+              <Card.Title>Card Title 3</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 }
