e2dcd9215b6ad887d0c50d945456cde9ceb8e76a
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-11-27 22:12:26 -0600

    stash fix

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductList from "./components/ProductList";
+
+function App() {
+    return(
+        <section>
+            <ProductList></ProductList>
+        </section>
+    );
+}
+
+export default App;
