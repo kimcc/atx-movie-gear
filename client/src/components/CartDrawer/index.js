14fc81cb7a7e6fafdcf532a7e24bf8afc3906963
kimcc <15278020+kimcc@users.noreply.github.com>
2021-11-30 13:36:06 -0600

    fix for overflowing datepicker

--- client/src/components/CartDrawer/index.js
+++ client/src/components/CartDrawer/index.js
@@ -17,6 +17,7 @@ function CartDrawer(props) {
   const [focusedInput, setFocusedInput] = useState();
 
   return (
+    <div className="cart-container">
     <div>
       <div className="flex-row space-between" style={{marginBottom: "24px"}}>
         <h2>Shopping Cart</h2>
@@ -47,6 +48,7 @@ function CartDrawer(props) {
                 onFocusChange={(focusedInput) => setFocusedInput(focusedInput)}
                 startDatePlaceholderText="Pickup date"
                 endDatePlaceholderText="Dropoff date"
+                numberOfMonths="1"
               />
 
             </div>
@@ -74,7 +76,7 @@ function CartDrawer(props) {
           </div>
 
           <div className="flex-column space-between" style={{marginTop: "40px"}}>
-            <div className="flex-row space-between">
+            <div className="flex-row space-between" style={{marginBottom: "24px"}}>
               <h5>Total</h5>
               <h3> ${props.calculateTotal}</h3>
             </div>
@@ -94,6 +96,7 @@ function CartDrawer(props) {
           You haven't added anything to your cart yet!
         </h6>
       )}
+    </div>
     </div>
   );
 }
